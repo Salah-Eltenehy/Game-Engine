a68619acff36e67713c07b8886a5639730f217db
Salah-Eltenehy <salaheltenehy@gmail.com>
2022-05-26 13:03:50 +0200

    Adding Chess, Xo, and Checker boards

---
+++ src/main/scala/ChessGUI/Checker.scala
@@ -0,0 +1,155 @@
+package ChessGUI
+
+import javafx.scene.paint.ImagePattern
+import scalafx.scene.image.Image
+import scalafx.scene.layout.GridPane
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
+
+class Checker {
+  var number_of_clickes: Int = 0
+  var first_click: String = ""
+  var second_click: String = ""
+  var gridPane= new GridPane
+  def initialize_checker(): GridPane = {
+    var i = 0
+    var j = 0
+    var white: Boolean = true
+    while(i < 8)
+    {
+      while (j < 8)
+      {
+        var rec2 = Rectangle(64, 64)
+        var rec = Rectangle(64, 64)
+        if(white)
+          {
+            rec2.fill = Color.White
+          }
+        else
+          rec2.fill = Color.Gray
+        white = !white
+        rec = setImages(rec, j, i, white)
+        gridPane.add(rec2, i, j)
+        gridPane.add(rec, i, j)
+        j = j+1
+      }
+      white = !white
+      j = 0
+      i = i+1
+    }
+    gridPane.setOnMouseClicked((e) => {
+      var raw: Int=  Math.floor(e.getSceneY/64).toInt
+      var col: Int = Math.floor(e.getSceneX/64).toInt
+      println(s"raw: $raw col: $col")
+      if (number_of_clickes == 1)
+      {
+        number_of_clickes = 0
+        second_click = raw + " " + col
+        var rec1 = Rectangle(64, 64)
+        rec1.fill = Color.Gray
+        var arr1 = first_click.split(" ")
+        gridPane.add(rec1, Integer.parseInt(arr1(1)), Integer.parseInt(arr1(0)))
+        var curDir :String =System.getProperty("user.dir")
+        var img = new Image(curDir+"\\imgs\\BLAACK.png")
+        var rec2 = Rectangle(64, 64)
+        rec2.setFill(new ImagePattern(img))
+        var arr2 = second_click.split(" ")
+        gridPane.add(rec2, Integer.parseInt(arr2(1)), Integer.parseInt(arr2(0)))
+      }
+      else
+      {
+        first_click = raw + " " + col
+        number_of_clickes = 1
+      }
+    })
+    gridPane
+  }
+  def setImages(rectangle: Rectangle, raw: Int, col: Int, color: Boolean): Rectangle = {
+    var curDir :String =System.getProperty("user.dir")
+    if (raw == 0 || raw == 2)
+      {
+        if (col%2== 1)
+        {
+          var img = new Image(curDir+"\\imgs\\BLAACK.png")
+          rectangle.setFill(new ImagePattern(img))
+        }
+        else {
+          if (!color)
+            rectangle.fill = Color.White
+          else
+            rectangle.fill = Color.Gray
+        }
+      }
+    else if(raw == 1)
+      {
+        if (col%2== 0)
+        {
+          var img = new Image(curDir+"\\imgs\\BLAACK.png")
+          rectangle.setFill(new ImagePattern(img))
+        }
+        else {
+          if (!color)
+            rectangle.fill = Color.White
+          else
+            rectangle.fill = Color.Gray
+        }
+      }
+    else if(raw == 5 || raw ==7)
+    {
+      if (col%2== 0)
+      {
+        var img = new Image(curDir+"\\imgs\\WHITE.png")
+        rectangle.setFill(new ImagePattern(img))
+      }
+      else {
+        if (!color)
+          rectangle.fill = Color.White
+        else
+          rectangle.fill = Color.Gray
+      }
+    }
+    else if(raw == 6)
+      {
+        if (col%2== 1)
+        {
+          var img = new Image(curDir+"\\imgs\\WHITE.png")
+          rectangle.setFill(new ImagePattern(img))
+        }
+        else {
+          if (!color)
+            rectangle.fill = Color.White
+          else
+            rectangle.fill = Color.Gray
+        }
+      }
+    else {
+      if (!color)
+        rectangle.fill = Color.White
+      else
+        rectangle.fill = Color.Gray
+    }
+    rectangle
+  }
+
+}
+/*
+gridPane.setOnMouseClicked((e) => {
+          var raw: Int=  Math.floor(e.getSceneY/64).toInt
+          var col: Int = Math.floor(e.getSceneX/64).toInt
+          println(s"raw: $raw col: $col number of clicks: $number_of_clickes")
+          if (number_of_clickes == 1)
+          {
+            number_of_clickes = 0
+            second_click = raw + " " + col
+
+          }
+          else
+          {
+            first_click = raw + " " + col
+            number_of_clickes = 1
+          }
+        })
+
+
+
+ */
---
+++ src/main/scala/ChessGUI/XO.scala
@@ -0,0 +1,50 @@
+package ChessGUI
+
+import javafx.scene.paint.ImagePattern
+import scalafx.scene.image.Image
+import scalafx.scene.layout.GridPane
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
+
+class XO {
+  var player: Int = 1
+  def initialize_XO(): GridPane =
+  {
+    var grid_pane = new GridPane()
+    var i: Int = 0
+    var j: Int = 0
+    while(i < 3) {
+      while (j < 3) {
+        var rectangle = Rectangle(100, 100)
+        rectangle.setStroke(Color.Green)
+        rectangle.setStrokeWidth(3)
+        grid_pane.add(rectangle, i, j)
+        j += 1
+      }
+      j = 0
+      i += 1
+    }
+    grid_pane.setOnMouseClicked((e) => {
+      var raw: Int=  Math.floor(e.getSceneY/100).toInt
+      var col: Int = Math.floor(e.getSceneX/100).toInt
+      var rectangle = Rectangle(100, 100)
+      rectangle.setStroke(Color.Green)
+      rectangle.setStrokeWidth(3)
+      var curDir :String =System.getProperty("user.dir")
+      if (player == 1)
+        {
+          var img = new Image(curDir+"\\imgs\\X1.PNG")
+          rectangle.setFill(new ImagePattern(img))
+          player = 2
+        }
+      else {
+        var img = new Image(curDir+"\\imgs\\O.jpeg")
+        rectangle.setFill(new ImagePattern(img))
+        player = 1
+      }
+      grid_pane.add(rectangle, col, raw)
+    })
+    grid_pane
+  }
+
+}
