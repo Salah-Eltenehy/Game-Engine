93f5a2c2c207277818931722713a94f000a9f6ec
Ahmed Hesham <67082298+Ahmed122000@users.noreply.github.com>
2022-05-28 15:43:55 +0200

    adding return start to screen button

--- src/main/scala/StartScreen.scala
+++ src/main/scala/StartScreen.scala
@@ -1,6 +1,9 @@
+import javafx.scene.control.Alert
+import javafx.scene.control.Alert.AlertType
 import javafx.scene.paint.ImagePattern
 import scalafx.application.JFXApp
 import scalafx.scene._
+import scalafx.scene.control.Button
 import scalafx.scene.image.Image
 import scalafx.scene.layout.GridPane
 import scalafx.scene.paint._
@@ -11,7 +14,7 @@ class StartScreen{
   def get_choice () : String = {
     var choice : String = ""
     var app = new JFXApp {
-
+      var startButton = new Button("Start Screen")
 
       stage = new JFXApp.PrimaryStage {
         val root = new Group
@@ -23,11 +26,19 @@ class StartScreen{
           var chess_img: Image = new Image(curDir+"\\imgs\\chess.PNG")
           chess_rectangle.setFill(new ImagePattern(chess_img))
           chess_rectangle.setOnMouseClicked((e) => {
+            var alert = new Alert(AlertType.NONE)
+            alert.setTitle("Promotion")
+            alert.setAlertType(AlertType.INFORMATION)
+            alert.setContentText("For Promotion\nQueen: q  Bishop: b\nRook: r  Knight: n\nNote:Case sensitive ")
+            alert.show()
             choice = "chess"
             var obj = new Board
-            stage.setWidth(640)
+            stage.setWidth(610)
             stage.setHeight(550)
-            content = List(obj.get_choice())
+            startButton.setLayoutX(515)
+            startButton.setLayoutY(130)
+
+            content = List(obj.get_choice(), startButton)
           })
           chess_rectangle.setX(30)
           chess_rectangle.setY(50)
@@ -38,9 +49,12 @@ class StartScreen{
           checker_rectangle.setOnMouseClicked((e) => {
             choice = "checker"
             var obj = new Checker
-            stage.setWidth(525)
+            stage.setWidth(610)
             stage.setHeight(550)
-            content = obj.initialize_checker()
+            startButton.setLayoutX(515)
+            startButton.setLayoutY(50)
+
+            content = List(obj.initialize_checker(), startButton)
           })
           checker_rectangle.setX(250)
           checker_rectangle.setY(50)
@@ -50,13 +64,16 @@ class StartScreen{
           connect4_rectangle.setFill(new ImagePattern(connect4_img))
           connect4_rectangle.setOnMouseClicked((e) => {
             choice = "connect4"
-            stage.setWidth(407)
+            stage.setWidth(490)
             stage.setHeight(400)
             var connect4 =new Connect4
             var connect4_rectangle =  Rectangle(392, 360)
             var connect4_img: Image = new Image(curDir+"\\imgs\\connect4_Board.PNG")
             connect4_rectangle.setFill(new ImagePattern(connect4_img))
-            content=List(connect4_rectangle,connect4.initialize_Connect4())
+            startButton.setLayoutX(400)
+            startButton.setLayoutY(50)
+
+            content=List(connect4_rectangle,connect4.initialize_Connect4(), startButton)
           })
           connect4_rectangle.setX(30)
           connect4_rectangle.setY(250)
@@ -68,18 +85,27 @@ class StartScreen{
             choice = "XO"
             var obj = new XO
             stage.title = "Tic Tac Toe"
-            stage.setWidth(325)
+            stage.setWidth(400)
             stage.setHeight(349)
-            content = obj.initialize_XO()
+            startButton.setLayoutX(310)
+            startButton.setLayoutY(50)
+            content = List(obj.initialize_XO(), startButton)
           })
           XO_rectangle.setX(250)
           XO_rectangle.setY(250)
-
+          startButton.setOnAction(e => {
+            stage.setWidth(380)
+            stage.setHeight(400)
+            content = List (back_ground_rectangle, chess_rectangle, connect4_rectangle, XO_rectangle, checker_rectangle )
+          })
+          ///////
+          //
           var back_ground_rectangle =  Rectangle(380, 400)
           var back_ground_img: Image = new Image(curDir+"\\imgs\\background-img.PNG")
           back_ground_rectangle.setFill(new ImagePattern(back_ground_img))
           content = List (back_ground_rectangle, chess_rectangle, connect4_rectangle, XO_rectangle, checker_rectangle )
         }
+        resizable = false
       }
     }
     app.main(Array(""))
