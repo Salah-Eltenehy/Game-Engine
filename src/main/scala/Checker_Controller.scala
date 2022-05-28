10fd001bc066cce0b00a2405be333c7b6ecbe6fc
Ahmed Hesham <67082298+Ahmed122000@users.noreply.github.com>
2022-05-28 03:02:09 +0200

    Update Checker_Controller.scala
    apply the full rules for the checkers

--- src/main/scala/Checker_Controller.scala
+++ src/main/scala/Checker_Controller.scala
@@ -2,167 +2,138 @@ import java.util.NoSuchElementException
 
 class Checker_Controller {
     //shape of the game board
-    var checkersBoard : Array[Array[String]] = Array(Array("_","_","_", "_", "_", "_" ,"_", "_"),
+    var checkersBoard : Array[Array[String]] = Array(Array("_","b","_", "b", "_", "b" ,"_", "b"),
+                                                     Array("b","_","b", "_", "b", "_" ,"b", "_"),
+                                                     Array("_","b","_", "b", "_", "b" ,"_", "b"),
                                                      Array("_","_","_", "_", "_", "_" ,"_", "_"),
                                                      Array("_","_","_", "_", "_", "_" ,"_", "_"),
-                                                     Array("_","_","_", "_", "_", "_" ,"_", "_"),
-                                                     Array("_","_","_", "_", "_", "_" ,"_", "_"),
-                                                     Array("_","_","_", "_", "_", "_" ,"_", "_"),
-                                                     Array("_","_","_", "_", "_", "_" ,"_", "_"),
-                                                     Array("_","_","_", "_", "_", "_" ,"_", "_")
+                                                     Array("w","_","w", "_", "w", "_" ,"w", "_"),
+                                                     Array("_","w","_", "w", "_", "w" ,"_", "w"),
+                                                     Array("w","_","w", "_", "w", "_" ,"w", "_")
     )
 
-  //list of the black pieces
-  val blackPieces : List[block]= List(new block("b", 0, 1),
-                                      new block("b", 0, 3),
-                                      new block("b", 0, 5),
-                                      new block("b", 0, 7),
-                                      new block("b", 1, 0),
-                                      new block("b", 1, 2),
-                                      new block("b", 1, 4),
-                                      new block("b", 1, 6),
-                                      new block("b", 2, 1),
-                                      new block("b", 2, 3),
-                                      new block("b", 2, 7),
-                                      new block("b", 2, 5)
-  )
+    var availableBlackAttack: Boolean = false //check if there any available black attack moves
+    var availableWhiteAttack: Boolean = false //check fi there any available white attack moves
 
-  //list of the white pieces
-  val whitePieces : List[block]= List(new block("w", 5, 0),
-                                      new block("w", 5, 2),
-                                      new block("w", 5, 4),
-                                      new block("w", 5, 6),
-                                      new block("w", 6, 1),
-                                      new block("w", 6, 3),
-                                      new block("w", 6, 5),
-                                      new block("w", 6, 7),
-                                      new block("w", 7, 0),
-                                      new block("w", 7, 2),
-                                      new block("w", 7, 4),
-                                      new block("w", 7, 6)
-  )
-
-
-    //set all white pieces on board
-    whitePieces.foreach(piece => checkersBoard(piece.position._1)(piece.position._2) = piece.color)
-
-    //set all black pieces on board
-    blackPieces.foreach(piece => checkersBoard(piece.position._1)(piece.position._2) = piece.color)
-
-  //check if the input is valid
-  def validateInput(input: String): Boolean= {
-    //check if the input in the availabe range
-    var valid : Boolean = input.matches("[a-hA-H][1-8][a-hA-H][1-8]")
-    valid
-  }
-
-  //turn string input into int co-ordinations
-  def translateInput(input : String): ((Int, Int), (Int, Int)) = {
-    //if the letters are case small -97, case capital - 65
-    var col1: Int = if(input.matches("[a-h]..."))input(0).toInt - 97
-                    else input(0).toInt - 65
-
-    var col2: Int = if(input.matches("[a-h]..."))input(2).toInt - 97
-                    else input(2).toInt - 65
-
-    //return tuple of the dimension pairs
-    ((input(1).toInt - 49, col1), (input(3).toInt - 49, col2))
-    }
+    //print the board
+    checkersBoard.foreach{x => x.foreach(print); println()}
 
   def validateMove(start: (Int, Int), end : (Int, Int), turn: Int) : Boolean = {
-    println(s"turn is: $turn")
-    //get the piece in the start position
-    var piece: block = try {
-      if (turn == 1) blackPieces.filter(_.position == start).head
-      else whitePieces.filter(_.position == start).head
-    }
-    catch {
-      //in case of empty square end the function --> no valid move
-      case a: NoSuchElementException => println("you chose empty square"); return false
-    }
+    println(s"now turn is: ${turn}")
 
-    //check if there is any pieces in the dist. position
-    var dist : List[block] = blackPieces.filter(_.position == end).concat(whitePieces.filter(_.position == end))
+    var piece: String = checkersBoard(start._1)(start._2) //get the piece the player choose
+
+    //check the input is valid square for each player
+    if((turn == 1 && (!piece.equals("b") && !piece.equals("sb"))) || (turn != 1 && (!piece.equals("w") && !piece.equals("sw"))) ) {
+      println("you choose empty square or non valid piece"); return false} //end the move with invalid if the player choose wrong square
+
+    //if destination is not empty then the move is not valid
+    if(!checkersBoard(end._1)(end._2).equals("_")) {
+      println("you chose un-empty square to move to, invalid move"); return false}
 
     //get horizontal distance for the chosen move
-    var x_space : Int = end._2 - start._2
+    val x_space : Int = end._2 - start._2
+    println(s"x space is: ${x_space}")
 
     //get vertical distance for the chosen move
-    var y_space : Int = end._1 - start._1
+    val y_space : Int = end._1 - start._1
+    println(s"y space is: ${y_space}")
 
     //boolean determine which player
     var player1 : Boolean = if(turn == 1) true
                             else false
 
+    println(s"is it player 1: ${player1.toString}")
+    println(s"is it player 2: ${(!player1).toString}")
+
     //check if the  piece is a king piece
-    var special : Boolean = if(piece.special) true
+    var special : Boolean = if(checkersBoard(start._1)(start._2).equals("sw") || checkersBoard(start._1)(start._2).equals("sb")) true
                             else false
+    println(s"this piece is special: $special")
+
     //general black piece move (go to the next row and check that dist. square is empty)
-    var blackMove : Boolean = player1 && y_space == 1 && dist.isEmpty
+    var blackMove : Boolean = player1 && y_space == 1 && !availableBlackAttack
+
     //general white piece move(go to the next row and check that dist. square is empty)
-    var whiteMove : Boolean = !player1 && y_space == -1 && dist.isEmpty
+    var whiteMove : Boolean = !player1 && y_space == -1 && !availableWhiteAttack
+    println(s"this move is small black move ${blackMove.toString} and is small white move ${whiteMove.toString}")
 
     //black piece attack movement constant condition
-    var blackAttack : Boolean = player1 && y_space == 2 && dist.isEmpty
+    var blackAttack : Boolean = player1 && y_space == 2 && (x_space == 2 || x_space == -2)
+
     //white piece attack movement constant condition
-    var whiteAttack : Boolean = !player1 &&(x_space == 2 || x_space == -2) && y_space == -2
+    var whiteAttack : Boolean = !player1 && y_space == -2 && (x_space == 2 || x_space == -2)
+    println(s"this is black attack move: ${blackAttack.toString}, and white attack move: ${whiteAttack.toString}")
 
     //special attack constant condition
-    var specialBlackAttack: Boolean = player1 && y_space == -2 && dist.isEmpty
-    var specialWhiteAttack: Boolean = !player1 && y_space == 2 && dist.isEmpty
+    var specialBlackAttack: Boolean = player1 && y_space == -2 && (x_space == 2 || x_space == -2)
+    var specialWhiteAttack: Boolean = !player1 && y_space == 2 && (x_space == 2 || x_space == -2)
+
+    println(s"this is going to be special move for black piece: ${specialBlackAttack.toString}, and it's special white piece: ${specialWhiteAttack}")
 
     var valid : Boolean = {
 
       /*one step moves*/
-      if (blackMove && !whitePieces.exists(_.position == (start._1+1, start._2+1)) && x_space == -1) true //black to left
-      else if(blackMove && !whitePieces.exists(_.position == (start._1+1, start._2-1)) && x_space == 1)true //black to right
-      else if(whiteMove && !blackPieces.exists(_.position == (start._1-1, start._2+1)) && x_space == -1) true //white to left
-      else if(whiteMove && !blackPieces.exists(_.position == (start._1-1, start._2-1))  && x_space == 1) true //white to right
+      if (blackMove && (x_space == -1 || x_space == 1)) true //black to left
+      else if(whiteMove && (x_space == -1 || x_space == 1  )) true //white to left
 
       /*two level steps*/
-      else if(blackAttack && whitePieces.exists(_.position == (start._1+1, start._2-1))  && x_space == -2) true
-      else if(blackAttack && whitePieces.exists(_.position == (start._1+1, start._2+1))  && x_space == 2) true
-      else if(whiteAttack && blackPieces.exists(_.position == (start._1-1, start._2-1))  && x_space == -2) true
-      else if(whiteAttack && blackPieces.exists(_.position == (start._1-1, start._2+1))  && x_space == 2) true
+      else if(blackAttack && checkersBoard((start._1 + end._1) / 2)((start._2 + end._2) / 2).equals("w") || checkersBoard((start._1 + end._1) / 2)((start._2 + end._2) / 2).equals("sw")) true
+      else if(whiteAttack && checkersBoard((start._1 + end._1) / 2)((start._2 + end._2) / 2).equals("b") || checkersBoard((start._1 + end._1) / 2)((start._2 + end._2) / 2).equals("sb")) true
 
       /*special moves*/
-      else if(specialBlackAttack && whitePieces.exists(_.position == (start._1-1, start._2-1)) && x_space == -2) true
-      else if(specialBlackAttack && whitePieces.exists(_.position == (start._1-1, start._2+1)) && x_space == 2) true
-      else if(specialWhiteAttack && blackPieces.exists(_.position == (start._1+1, start._2-1)) && x_space == -2) true
-      else if(specialWhiteAttack && blackPieces.exists(_.position == (start._1+1, start._2+1)) && x_space == 2) true
+      else if(specialBlackAttack && checkersBoard((start._1 + end._1) / 2)((start._2 + end._2) / 2).equals("w") || checkersBoard((start._1 + end._1) / 2)((start._2 + end._2) / 2).equals("sw") ) true
+      else if(specialWhiteAttack && checkersBoard((start._1 + end._1) / 2)((start._2 + end._2) / 2).equals("b") || checkersBoard((start._1 + end._1) / 2)((start._2 + end._2) / 2).equals("sb") ) true
 
       //invalid move
       else false
     }
 
+    println(s"now the move is valid: ${valid.toString}")
     valid
   }
 
   def movePiece(start: (Int, Int),end:(Int, Int), player:Int): Unit = {
-    /*get the piece to move*/
-    println(s"the player now is $player and the piece is found ${blackPieces.exists(_.position == start).toString}")
-    var piece:block = if(player == 1) blackPieces.filter(_.position == start).head
-                      else whitePieces.filter(_.position == start).head
 
-    /*change piece position*/
-    piece.position = end
-
-    /*show the move on the board*/
+    checkersBoard(end._1)(end._2) = checkersBoard(start._1)(start._2)
     checkersBoard(start._1)(start._2) = "_"
-    checkersBoard(end._1)(end._2) = piece.color
+
 
     //in case it was attack move
     if(math.abs(end._1 - start._1) == 2)
     {
       //empty the attacked square
+      if(checkersBoard((start._1 + end._1) /2)((start._2 + end._2) /2).equals("w") || checkersBoard((start._1 + end._1) /2)((start._2 + end._2) /2).equals("sw")) availableBlackAttack = false
+      if(checkersBoard((start._1 + end._1) /2)((start._2 + end._2) /2).equals("b") || checkersBoard((start._1 + end._1) /2)((start._2 + end._2) /2).equals("sb")) availableWhiteAttack = false
       checkersBoard((start._1 + end._1) /2)((start._2 + end._2) /2) = "_"
-      //remove any piece in the square and put it out of the board
-      whitePieces.filter(_.position == ((start._1 + end._1) /2, (start._2 + end._2) /2)).foreach(_.position = (-1, -1))
-      whitePieces.filter(_.position == ((start._1 + end._1) /2, (start._2 + end._2) /2)).foreach(_.position = (-1, -1))
+
     }
 
     /*check if there any king pieces after each move*/
-    if(player == 1 && end._1 == 7)piece.special = true
-    else if(player == 2 && end._1 == 0) piece.special = true
+    if(player == 1 && end._1 == 7) {checkersBoard(end._1)(end._2) = "sb"}
+    else if(player == 2 && end._1 == 0) {checkersBoard(end._1)(end._2)= "sw"}
+
+    checkersBoard.foreach{x => x.foreach(print); println()}
+
+    availableWhiteAttack = if(player ==1 && checkersBoard(end._1)(end._2).equals("b") && end._1 < 7 && end._1 > 0 && end._2 < 7 && end._2 > 0){
+      if((checkersBoard(end._1 + 1)(end._2 + 1).equals("w") && checkersBoard(end._1 - 1)(end._2 - 1).equals("_")) ||
+         (checkersBoard(end._1 + 1)(end._2 - 1).equals("w") && checkersBoard(end._1 - 1)(end._2 + 1).equals("_")) ||
+         (checkersBoard(end._1 - 1)(end._2 + 1).equals("sw") && checkersBoard(end._1 + 1)(end._2 - 1).equals("_")) ||
+         (checkersBoard(end._1 - 1)(end._2 - 1).equals("sw") && checkersBoard(end._1 + 1)(end._2 + 1).equals("_"))) true
+      else false
+    } else false
+
+    availableBlackAttack = if(player !=1 && checkersBoard(end._1)(end._2).equals("w") && end._1 < 7 && end._1 > 0 && end._2 < 7 && end._2 > 0){
+      if((checkersBoard(end._1 - 1)(end._2 + 1).equals("b") && checkersBoard(end._1 + 1)(end._2 - 1).equals("_")) ||
+         (checkersBoard(end._1 - 1)(end._2 - 1).equals("b") && checkersBoard(end._1 + 1)(end._2 + 1).equals("_")) ||
+         (checkersBoard(end._1 + 1)(end._2 + 1).equals("sb") && checkersBoard(end._1 - 1)(end._2 - 1).equals("_")) ||
+         (checkersBoard(end._1 + 1)(end._2 - 1).equals("sb") && checkersBoard(end._1 - 1)(end._2 + 1).equals("_"))) true
+      else false
+    } else false
+
   }
 }
+
+
+
+
